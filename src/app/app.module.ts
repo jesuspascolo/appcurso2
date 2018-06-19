bef3fe9249dc63b28d7695dac032d89f1a9fb07c
Gonzalez Pascolo <jesus-david.gonzalezpascolo.external@atos.net>
2018-06-19 16:20:04 +0200

    Formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProveedoresService } from './servicios/proveedores.service';
 import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { HeaderComponent } from './header/header.component';
-
+import { AddproveeComponent } from './proveedores/addprovee/addprovee.component';
+import { AddpresComponent } from './presupuestos/addpres/addpres.component';
+//La ruta default '**' siempre al final
 const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'proveedores', component: ProveedoresComponent },
+  { path: 'addprovee', component: AddproveeComponent },
+  { path: 'addpres', component: AddpresComponent },
   { path: '**', component: InicioComponent }
 ];
 
@@ -19,11 +25,15 @@ const routes: Routes = [
     AppComponent,
     ProveedoresComponent,
     InicioComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddproveeComponent,
+    AddpresComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes) //Estableciendo un array de rutas que empleará nuestra página
+    RouterModule.forRoot(routes),//Estableciendo un array de rutas que empleará nuestra página
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [ProveedoresService],
   bootstrap: [AppComponent]
